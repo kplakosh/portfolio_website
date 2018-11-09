69bd6b2ea624b99bc65fc207a850a5024a15d360
kplakosh <kplakosh@gmail.com>
2018-11-08 23:01:55 -0800

    code refactoring

--- shared.js
+++ shared.js
@@ -4,9 +4,14 @@ var toggleButton = document.querySelector('.toggle-button');
 
 backdrop.addEventListener('click', function() {
     mobileNav.classList.remove('open');
+    closeBackdrop();
 });
 
 toggleButton.addEventListener("click", function () {
     mobileNav.classList.add("open");
     backdrop.classList.add("open");
 });
+
+function closeBackdrop() {
+    backdrop.classList.remove("open");
+}
