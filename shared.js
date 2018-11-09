e109e6cc8133846d8fe9d8aa82b46f2a1be520f8
kplakosh <kplakosh@gmail.com>
2018-11-08 22:03:30 -0800

    humburger menu open implemented.

--- shared.js
+++ shared.js
@@ -1,8 +1,12 @@
 var backdrop = document.querySelector('.backdrop');
 var mobileNav = document.querySelector('.mobile-nav');
-
-
+var toggleButton = document.querySelector('.toggle-button');
 
 backdrop.addEventListener('click', function() {
     mobileNav.classList.remove('open');
+});
+
+toggleButton.addEventListener("click", function () {
+    mobileNav.classList.add("open");
+    backdrop.classList.add("open");
 });
