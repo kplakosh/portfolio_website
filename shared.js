abc630f5f9c8a611b736022c431e769e3e035991
kplakosh <kplakosh@gmail.com>
2019-04-08 00:16:28 -0700

    update design and portfolio section added

--- shared.js
+++ shared.js
@@ -2,16 +2,16 @@ var backdrop = document.querySelector('.backdrop');
 var mobileNav = document.querySelector('.mobile-nav');
 var toggleButton = document.querySelector('.toggle-button');
 
-backdrop.addEventListener('click', function() {
-    mobileNav.classList.remove('open');
-    closeBackdrop();
-});
+// backdrop.addEventListener('click', function() {
+//     mobileNav.classList.remove('open');
+//     closeBackdrop();
+// });
 
-toggleButton.addEventListener("click", function () {
-    mobileNav.classList.add("open");
-    backdrop.classList.add("open");
-});
+// toggleButton.addEventListener("click", function () {
+//     mobileNav.classList.add("open");
+//     backdrop.classList.add("open");
+// });
 
-function closeBackdrop() {
-    backdrop.classList.remove("open");
-}
+// function closeBackdrop() {
+//     backdrop.classList.remove("open");
+// }
