c62f8d0e6b9be5e560a76098bcc9655439f85a21
kplakosh <kplakosh@gmail.com>
2019-04-12 21:40:46 -0700

    backdrop added

--- shared.js
+++ shared.js
@@ -1,20 +1,21 @@
-// var backdrop = document.querySelector('.backdrop');
-// var mobileNav = document.querySelector('.mobile-nav');
-// var toggleButton = document.querySelector('.toggle-button');
+var backdrop = document.querySelector('.backdrop');
+var mobileNav = document.querySelector('.mobile-nav');
+var toggleButton = document.querySelector('.toggle-button');
 
-// backdrop.addEventListener('click', function() {
-//     mobileNav.classList.remove('open');
-//     closeBackdrop();
-// });
+backdrop.addEventListener('click', function() {
+    mobileNav.classList.remove('open');
+    closeBackdrop();
+});
 
-// toggleButton.addEventListener("click", function () {
-//     mobileNav.classList.add("open");
-//     backdrop.style.display = "block";
-//     setTimeout(() => {
-//         backdrop.classList.add("open");
-//     }, 10);
-// });
+toggleButton.addEventListener("click", function () {
+    mobileNav.classList.add("open");
+    backdrop.style.display = "block";
+    setTimeout(() => {
+        backdrop.classList.add("open");
+    }, 10);
+});
 
-// function closeBackdrop() {
-//     backdrop.classList.remove("open");
-// }
+function closeBackdrop() {
+    backdrop.classList.remove("open");
+    backdrop.style.display = "none";
+}
