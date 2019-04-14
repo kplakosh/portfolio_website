6aca99441e69a92f097b1baf15fa208b6c62672d
kplakosh <kplakosh@gmail.com>
2019-04-13 22:39:26 -0700

    contact me information added

--- shared.js
+++ shared.js
@@ -5,19 +5,19 @@ var toggleButton = document.querySelector('.toggle-button');
 backdrop.addEventListener('click', function() {
     mobileNav.classList.remove('open');
     closeBackdrop();
-    toggleButton.style.display = "block";
+    toggleButton.style.display = 'block';
 });
 
 toggleButton.addEventListener("click", function () {
-    toggleButton.style.display = "none";
-    mobileNav.classList.add("open");
-    backdrop.style.display = "block";
+    toggleButton.style.display = 'none';
+    mobileNav.classList.add('open');
+    backdrop.style.display = 'block';
     setTimeout(() => {
-        backdrop.classList.add("open");
+        backdrop.classList.add('open');
     }, 10);
 });
 
 function closeBackdrop() {
-    backdrop.classList.remove("open");
-    backdrop.style.display = "none";
+    backdrop.classList.remove('open');
+    backdrop.style.display = 'none';
 }
